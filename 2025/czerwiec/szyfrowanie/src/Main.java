98010aa970e8f2240be7fd908ce2384fb29b58ba
SchoolWB <103361149+SchoolWG@users.noreply.github.com>
2025-11-16 19:28:55 +0100

    Delete 2025/czerwiec/szyfrowanie directory

--- 2025/czerwiec/szyfrowanie/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        szyfrCezara ss = new szyfrCezara();
-
-        ss.szyfruj("z", 10);
-        System.out.println(ss.szyfruj("ala", 3));
-    }
-}
